bde3075ee2c8eb802add08190c987ec5e61fe466
ziondayoo <adetunjitemidayo26@gmail.com>
2022-10-12 00:46:18 +0100

    shop-online-app

---
+++ ShopOnline.Web/Pages/ShoppingCartBase.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using ShopOnline.Models.Dtos;
+using ShopOnline.Web.Services;
+using ShopOnline.Web.Services.Contracts;
+
+namespace ShopOnline.Web.Pages
+{
+    public class ShoppingCartBase:ComponentBase
+    {
+        [Inject]
+        public IJSRuntime Js { get; set; }
+        [Inject]
+        public IShoppingCartService ShoppingCartService { get; set; }
+        public List<CartItemDto> ShoppingCartItems { get; set; }
+        public string ErrorMessage { get; set; }
+        protected string TotalPrice { get; set; }
+        protected int TotalQuantity { get; set; }
+        protected override async Task OnInitializedAsync()
+        {
+            try
+            {
+                ShoppingCartItems = await ShoppingCartService.GetItems(HardCoded.UserId);
+                CalculateCartSummaryTotals();
+
+            }
+            catch (Exception ex)
+            {
+
+                ErrorMessage = ex.Message;
+            }
+        }
+        protected async Task DeleteCartItem_Click(int id)
+        {
+            var cartItemDto = await ShoppingCartService.DeleteItem(id);
+            RemoveCartItem(id);
+            CalculateCartSummaryTotals();
+
+
+        }
+        protected async Task UpdateQtycartItem_Click(int id,int qty)
+        {
+            try
+            {
+                if(qty > 0)
+                {
+                    var updateItemDto = new CartItemQtyUpdateDto
+                    {
+                        CartItemId = id,
+                        Qty = qty
+                    };
+                    var returnedUpdateItemDto = await this.ShoppingCartService.UpdateQty(updateItemDto);
+                    UpdateItemTotalPrice(returnedUpdateItemDto);
+                    CalculateCartSummaryTotals();
+                    await MakeUpdateQtyButtonVisible(id, false);
+
+                }
+                else
+                {
+                    var item = this.ShoppingCartItems.FirstOrDefault(i => i.Id == id);  
+                    if(item != null)
+                    {
+                        item.Qty = 1;
+                        item.TotalPrice = item.Price;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private  async Task MakeUpdateQtyButtonVisible(int id, bool visible)
+        {
+            await Js.InvokeVoidAsync("MakeUpdateQtyButtonVisible", id, visible);
+        }
+
+        protected async Task UpdateQty_Input(int id)
+        {
+            await MakeUpdateQtyButtonVisible(id, true);
+
+        }
+        private void UpdateItemTotalPrice(CartItemDto cartItemDto)
+        {
+            var item = GetCartItem(cartItemDto.Id);
+            if(item != null)
+            {
+                item.TotalPrice = cartItemDto.Price * cartItemDto.Qty;
+            }
+        }
+        private void CalculateCartSummaryTotals()
+        {
+            SetTotalPrice();
+            SetTotalQuantity();
+        }
+        private void SetTotalPrice()
+        {
+            TotalPrice = this.ShoppingCartItems.Sum(p => p.TotalPrice).ToString("C");
+        }
+        private void SetTotalQuantity()
+        {
+            TotalQuantity = this.ShoppingCartItems.Sum(p => p.Qty); 
+        }
+        private CartItemDto GetCartItem(int id)
+        {
+            return ShoppingCartItems.FirstOrDefault(i => i.Id == id);
+        }
+        private void RemoveCartItem(int id)
+        {
+            var cartItemDto = GetCartItem(id);
+            ShoppingCartItems.Remove(cartItemDto);
+        }
+    }
+}
---
+++ ShopOnline.Web/Services/IShoppingCartService.cs
@@ -0,0 +1,15 @@
+﻿using ShopOnline.Models.Dtos;
+
+namespace ShopOnline.Web.Services
+{
+    public interface IShoppingCartService
+    {
+        Task<List<CartItemDto>> GetItems(int userId);
+        Task<CartItemDto> AddItem(CartItemToAddDto cartItemToAddDto);
+        Task<CartItemDto> DeleteItem(int id);
+        Task<CartItemDto> UpdateQty(CartItemQtyUpdateDto cartItemQtyUpdateDto);
+
+        event Action<int> OnShoppingCartChanged;
+        void RaiseEventOnShoppingCartChanged(int totalQty);
+    }
+}
